3b55231914549c8c8037de9bb56feeebb82c349f
Adam Pociejowski <valverde12345@gmail.com>
2022-08-19 17:26:43 +0200

    Added section4 tasks

---
+++ src/test/java/com/vavlerde/masterofcode/section4/ExistInArrayPairWithGivenSum.java
@@ -0,0 +1,65 @@
+package com.vavlerde.masterofcode.section4;
+
+import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExistInArrayPairWithGivenSum {
+
+    /*
+    Write function that checks if in array existing pair of given sum
+     */
+    @Test
+    void shouldReturnTrue_whenExistsPairWithGivenSum() {
+        int[] values = {2, 4, 5, 7, 8};
+        assertTrue(bruteForceSolution(values, 11));
+        assertTrue(hashSetSolution(values, 11));
+    }
+
+    @Test
+    void shouldReturnTrue_whenExistsPairWithGivenSum2() {
+        int[] values = {2, 4, 5, 4, 8};
+        assertTrue(bruteForceSolution(values, 8));
+        assertTrue(hashSetSolution(values, 8));
+    }
+
+    @Test
+    void shouldReturnFalse_whenNotExistsPairWithGivenSum() {
+        int[] values = {2, 4, 5, 1, 8};
+        assertFalse(bruteForceSolution(values, 8));
+        assertFalse(hashSetSolution(values, 8));
+    }
+
+    @Test
+    void shouldReturnFalse_whenListHasOnlyOneItem() {
+        int[] values = {8};
+        assertFalse(bruteForceSolution(values, 8));
+        assertFalse(hashSetSolution(values, 8));
+    }
+
+    // O(n^2)
+    private boolean bruteForceSolution(final int[] values, final int sum) {
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+                if (i > j && values[i] + values[j] == sum) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // O(n)
+    private boolean hashSetSolution(final int[] values, final int sum) {
+        HashSet<Integer> alreadyFoundValues = new HashSet<>();
+        for (int v : values) {
+            if (alreadyFoundValues.contains(sum - v)) {
+                return true;
+            }
+            alreadyFoundValues.add(v);
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/com/vavlerde/masterofcode/section4/HasArraysCommonElement.java
@@ -0,0 +1,62 @@
+package com.vavlerde.masterofcode.section4;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HasArraysCommonElement {
+
+    /*
+    Write function that checks if 2 lists has any common element
+     */
+    @Test
+    void shouldReturnTrue_whenListsAreEqual() {
+        String[] arr1 = {"a", "b", "c"};
+        String[] arr2 = {"a", "b", "c"};
+        assertTrue(bruteForceSolution(arr1, arr2));
+        assertTrue(hashSetSolution(arr1, arr2));
+    }
+
+    @Test
+    void shouldReturnFalse_whenListsHasNoCommonElements() {
+        String[] arr1 = {"a", "b", "c", "c"};
+        String[] arr2 = {"d", "e", "f"};
+        assertFalse(bruteForceSolution(arr1, arr2));
+        assertFalse(hashSetSolution(arr1, arr2));
+    }
+
+    @Test
+    void shouldReturnTrue_whenListsHasOneCommonElement() {
+        String[] arr1 = {"a", "b", "c"};
+        String[] arr2 = {"c", "e", "f"};
+        assertTrue(bruteForceSolution(arr1, arr2));
+        assertTrue(hashSetSolution(arr1, arr2));
+    }
+
+    // O(n*m)
+    private boolean bruteForceSolution(final String[] arr1, final String[] arr2) {
+        for (String e1 : arr1) {
+            for (String e2 : arr2) {
+                if (e1.equals(e2)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // O(n+m)
+    private boolean hashSetSolution(final String[] arr1, final String[] arr2) {
+        final HashSet<String> arr1Set = new HashSet<>(Arrays.asList(arr1));
+        for (String e : arr2) {
+            if (arr1Set.contains(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
