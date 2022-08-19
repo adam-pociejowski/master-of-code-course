e6f8baef3179afd0986d68fbe9cc96cee2f532c6
Adam Pociejowski <valverde12345@gmail.com>
2022-08-19 16:50:05 +0200

    Initial commit

---
+++ src/main/java/com/vavlerde/masterofcode/MasterOfCodeCourseApplication.java
@@ -0,0 +1,13 @@
+package com.vavlerde.masterofcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterOfCodeCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterOfCodeCourseApplication.class, args);
+    }
+
+}
