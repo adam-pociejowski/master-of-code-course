c3a205a672cb6f14ebb4bd5392142b32544c8994
adampociejowski <valverde12345@gmail.com>
2022-12-20 07:34:46 +0100

    advent of code day 16

--- src/test/java/com/valverde/adventofcode2022/Day16.java
+++ src/test/java/com/valverde/adventofcode2022/Day16.java
@@ -1,27 +1,228 @@
 package com.valverde.adventofcode2022;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day16 extends AbstractTask {
 
-    @BeforeEach
-    void setup() {
+    @Test
+    void solution2() {
+        final List<String> lines = readStringLines("input16.txt");
+        Map<String, Node> nodes = getNodes(lines);
+        addConnections(lines, nodes);
+        Deque<State> queue = new ArrayDeque<>();
+        State startState = new State(0, 0, nodes.get("AA"), nodes.get("AA"), new HashSet<>());
+        State best = startState;
+        queue.add(startState);
+        int depth = 0;
+        Set<State> seen = new HashSet<>();
+        int minutes = 26;
+        while (!queue.isEmpty()) {
+            State state = queue.poll();
+            if (state.time > minutes ||
+                    seen.contains(state) ||
+                    (depth > 10 && state.score < 1400) ||
+                    (depth > 15 && state.score < 2000) ||
+                    (depth > 20 && state.score < 2300)) {
+                continue;
+            }
+            if (state.time > depth) {
+                seen = new HashSet<>();
+                depth = state.time;
+                System.out.printf("################### DEPTH: %s, BEST: %s, QUEUE: %s\n", depth, best, queue.size());
+            }
+            seen.add(state);
+            if (state.score > best.score) {
+                best = state;
+                System.out.printf("FOUND BETTER SCORE, DEPTH: %s, BEST: %s\n", depth, best);
+            }
+            if (!state.opened.contains(state.currentNode.name) && state.currentNode.rate > 0 &&
+                    !state.opened.contains(state.currentNodeElephant.name) && state.currentNodeElephant.rate > 0 &&
+                        !state.currentNode.equals(state.currentNodeElephant)) { //both open nodes
+                HashSet<String> opened = new HashSet<>(state.opened);
+                opened.add(state.currentNode.name);
+                opened.add(state.currentNodeElephant.name);
+                queue.add(new State(
+                        state.time + 1,
+                        state.score + (state.currentNode.rate * (minutes - state.time - 1)) + (state.currentNodeElephant.rate * (minutes - state.time - 1)),
+                        state.currentNode,
+                        state.currentNodeElephant,
+                        opened));
+            }
+            if (!state.opened.contains(state.currentNode.name) && state.currentNode.rate > 0) { //me open elephant moving
+                for (Node n : state.currentNodeElephant.connections) {
+                    HashSet<String> opened = new HashSet<>(state.opened);
+                    opened.add(state.currentNode.name);
+                    queue.add(new State(
+                            state.time + 1,
+                            state.score + (state.currentNode.rate * (minutes - state.time - 1)),
+                            state.currentNode,
+                            n,
+                            opened));
+                }
+            }
+            if (!state.opened.contains(state.currentNodeElephant.name) && state.currentNodeElephant.rate > 0) { //me moving, elephant open
+                for (Node n : state.currentNode.connections) {
+                    HashSet<String> opened = new HashSet<>(state.opened);
+                    opened.add(state.currentNodeElephant.name);
+                    queue.add(new State(
+                            state.time + 1,
+                            state.score + (state.currentNodeElephant.rate * (minutes - state.time - 1)),
+                            n,
+                            state.currentNodeElephant,
+                            opened));
+                }
+            }
+            for (Node eN : state.currentNodeElephant.connections) { //me moving, elephant moving
+                for (Node n : state.currentNode.connections) {
+                    queue.add(new State(
+                            state.time + 1,
+                            state.score,
+                            n,
+                            eN,
+                            new HashSet<>(state.opened)));
+                }
+            }
+        }
+
+        int result = best.score;
+        System.out.println("SOLUTION 2: " + result);
     }
 
     @Test
     void solution1() {
-        final List<String> lines = readStringLines("input9.txt");
-        int result = 0;
-        System.out.println("SOLUTION 1: "+result);
+        final List<String> lines = readStringLines("input16.txt");
+        Map<String, Node> nodes = getNodes(lines);
+        addConnections(lines, nodes);
+        Deque<State> queue = new ArrayDeque<>();
+        State startState = new State(0, 0, nodes.get("AA"), new HashSet<>());
+        State best = startState;
+        queue.add(startState);
+        int depth = 0;
+        Set<State> seen = new HashSet<>();
+        int minutes = 30;
+        while (!queue.isEmpty()) {
+            State state = queue.poll();
+            if (state.time > minutes ||
+                    seen.contains(state) ||
+                    (depth > 15 && state.score < 1000)) {
+                continue;
+            }
+            if (state.time > depth) {
+                seen = new HashSet<>();
+                depth = state.time;
+                System.out.printf("################### DEPTH: %s, BEST: %s, QUEUE: %s\n", depth, best, queue.size());
+            }
+            seen.add(state);
+            if (state.score > best.score) {
+                best = state;
+                System.out.printf("FOUND BETTER SCORE, DEPTH: %s, BEST: %s\n", depth, best);
+            }
+            if (!state.opened.contains(state.currentNode.name) && state.currentNode.rate > 0) { //open node
+                HashSet<String> opened = new HashSet<>(state.opened);
+                opened.add(state.currentNode.name);
+                queue.add(new State(
+                        state.time + 1,
+                        state.score + (state.currentNode.rate * (minutes - state.time - 1)),
+                        state.currentNode,
+                        opened));
+            }
+            for (Node n : state.currentNode.connections) { //visit node
+                queue.add(new State(
+                        state.time + 1,
+                        state.score,
+                        n,
+                        new HashSet<>(state.opened)));
+            }
+        }
+
+        int result = best.score;
+        System.out.println("SOLUTION 1: " + result);
     }
 
-    @Test
-    void solution2() {
-        final List<String> lines = readStringLines("input9.txt");
-        int result = 0;
-        System.out.println("SOLUTION 2: "+result);
+    private Map<String, Node> getNodes(List<String> lines) {
+        Map<String, Node> nodes = lines
+                .stream()
+                .map(l -> {
+                    String[] words = l.split(" ");
+                    String rate = words[4].split("=")[1].replace(";", "");
+                    return new Node(words[1], Integer.parseInt(rate));
+                })
+                .collect(Collectors.toMap(Node::getName, a -> a));
+        return nodes;
+    }
+
+    private void addConnections(List<String> lines, Map<String, Node> nodes) {
+        lines.forEach(l -> {
+            String currentNode = l.split(" ")[1];
+            String[] nodeNames = l.split(";")[1]
+                    .replaceAll(" tunnels lead to valves ", "")
+                    .replaceAll(" tunnel leads to valve ", "")
+                    .replaceAll(" ", "")
+                    .split(",");
+            Node node = nodes.get(currentNode);
+            for (String name : nodeNames) {
+                Node n = nodes.get(name);
+                node.connections.add(n);
+            }
+        });
+    }
+
+    static class State {
+        int time;
+        int score;
+        Node currentNode;
+        Node currentNodeElephant;
+        Set<String> opened;
+
+        public State(int time, int score, Node currentNode, Node currentNodeElephant, Set<String> opened) {
+            this.time = time;
+            this.score = score;
+            this.currentNode = currentNode;
+            this.currentNodeElephant = currentNodeElephant;
+            this.opened = opened;
+        }
+
+        public State(int time, int score, Node currentNode, Set<String> opened) {
+            this.time = time;
+            this.score = score;
+            this.currentNode = currentNode;
+            this.opened = opened;
+        }
+
+        @Override
+        public String toString() {
+            return "score="+score+", opened=" + opened;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            State state = (State) o;
+            return time == state.time && score == state.score && Objects.equals(currentNode, state.currentNode) && Objects.equals(currentNodeElephant, state.currentNodeElephant) && Objects.equals(opened, state.opened);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(time, score, currentNode, currentNodeElephant, opened);
+        }
+    }
+
+    static class Node {
+        String name;
+        int rate;
+        Set<Node> connections = new HashSet<>();
+
+
+        public Node(String name, int rate) {
+            this.rate = rate;
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
--- src/test/java/com/valverde/adventofcode2022/Day19.java
+++ src/test/java/com/valverde/adventofcode2022/Day19.java
@@ -3,44 +3,114 @@ package com.valverde.adventofcode2022;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 
 public class Day19 extends AbstractTask {
 
     @BeforeEach
-    void setup() {
-    }
+    void setup() {}
 
     @Test
     void solution2() {
-        final List<String> lines = readStringLines("inputTest.txt");
-        int result = 0;
+        List<BluePrint> bluePrints = getBluePrints("input19.txt");
+        int result = 1;
+        for (BluePrint bluePrint : bluePrints.subList(0, 3)) {
+            State best = null;
+            final Queue<State> queue = new ArrayDeque<>();
+            final Set<State> seen = new HashSet<>();
+            queue.add(new State(bluePrint));
+            long iterations = 0;
+            int depth = 0;
+            while (!queue.isEmpty() && iterations < 1000000000) {
+                State state = queue.poll();
+                if (state.time > 32) {
+                    continue;
+                }
+                if (seen.contains(state)) {
+                    continue;
+                }
+                if (state.time > depth) {
+                    depth = state.time;
+                    seen.removeIf(next -> next.time < state.time);
+                    System.out.println("DEPTH: "+depth+", SEEN: "+seen.size()+", QUEUE: "+queue.size()+", BEST: "+best);
+                }
+                seen.add(state);
+                iterations++;
+                if (Objects.isNull(best) || state.openedGeodes > best.openedGeodes) {
+                    best = state;
+                }
+                if (state.openedGeodes + 1 < best.openedGeodes ||
+                        (depth > 25 && state.clayRobots < 3)  ||
+                        (depth > 25 && state.obsidianRobots < 3)  ||
+                        (depth > 25 && state.oreRobots < 2)) { // heuristic to min number of states (out of memory)
+                    continue;
+                }
+                queue.add(state.doNothing());
+                if (state.ore >= state.bluePrint.oreRobot.ore) {
+                    queue.add(state.buildOreRobot());
+                }
+                if (state.ore >= state.bluePrint.clayRobot.ore) {
+                    queue.add(state.buildClayRobot());
+                }
+                if (state.ore >= state.bluePrint.obsidianRobot.ore && state.clay >= state.bluePrint.obsidianRobot.clay) {
+                    queue.add(state.buildObsidianRobot());
+                }
+                if (state.ore >= state.bluePrint.geodeRobot.ore && state.obsidian >= state.bluePrint.geodeRobot.obsidian) {
+                    queue.add(state.buildGeodeRobot());
+                }
+            }
+            System.out.printf("%s: %s\n", iterations, best);
+            result *= best.openedGeodes;
+        }
         System.out.println("SOLUTION 2: " + result);
+        //3080
     }
 
     @Test
     void solution1() {
-        List<BluePrint> bluePrints = getBluePrints("inputTest.txt");
+        List<BluePrint> bluePrints = getBluePrints("input19.txt");
+        int result = 0;
         for (BluePrint bluePrint : bluePrints) {
-            Simulation simulation = new Simulation(bluePrint);
-            for (int i = 0; i < 24; i++) {
-                int oreRobots = simulation.oreRobots;
-                int clayRobots = simulation.clayRobots;
-                int obsidianRobots = simulation.obsidianRobots;
-                int geodeRobots = simulation.geodeRobots;
-                simulation.buildGeodeRobot();
-                simulation.buildObsidianRobot();
-                simulation.buildClayRobot();
-                simulation.buildOreRobot();
-                simulation.ore += oreRobots;
-                simulation.clay += clayRobots;
-                simulation.obsidian += obsidianRobots;
-                simulation.openedGeodes += geodeRobots;
-                System.out.printf("%s: %s\n", (i + 1), simulation);
+            State best = null;
+            final Queue<State> queue = new ArrayDeque<>();
+            final Set<State> seen = new HashSet<>();
+            queue.add(new State(bluePrint));
+            long iterations = 0;
+            while (!queue.isEmpty() && iterations < 500000000) {
+                State state = queue.poll();
+                if (state.time > 24) {
+                    continue;
+                }
+                if (seen.contains(state)) {
+                    continue;
+                }
+                if (queue.size() > 20000000) {
+                    continue;
+                }
+                seen.add(state);
+                iterations++;
+                if (Objects.isNull(best) || state.openedGeodes > best.openedGeodes) {
+                    best = state;
+                    System.out.printf("%s: %s\n", iterations, state);
+                }
+                queue.add(state.doNothing());
+                if (state.ore >= state.bluePrint.oreRobot.ore) {
+                    queue.add(state.buildOreRobot());
+                }
+                if (state.ore >= state.bluePrint.clayRobot.ore) {
+                    queue.add(state.buildClayRobot());
+                }
+                if (state.ore >= state.bluePrint.obsidianRobot.ore && state.clay >= state.bluePrint.obsidianRobot.clay) {
+                    queue.add(state.buildObsidianRobot());
+                }
+                if (state.ore >= state.bluePrint.geodeRobot.ore && state.obsidian >= state.bluePrint.geodeRobot.obsidian) {
+                    queue.add(state.buildGeodeRobot());
+                }
             }
+            result += (bluePrints.indexOf(bluePrint) + 1) * best.openedGeodes;
         }
-        int result = bluePrints.size();
         System.out.println("SOLUTION 1: " + result);
+        //1147
     }
 
     private List<BluePrint> getBluePrints(String input) {
@@ -65,59 +135,109 @@ public class Day19 extends AbstractTask {
                 .toList();
     }
 
-    static class Simulation {
+    static class State {
         int ore = 0;
         int clay = 0;
         int obsidian = 0;
         int openedGeodes = 0;
-        BluePrint bluePrint;
         int oreRobots = 1;
         int clayRobots = 0;
         int obsidianRobots = 0;
         int geodeRobots = 0;
+        int time = 0;
+        BluePrint bluePrint;
 
-        public Simulation(BluePrint bluePrint) {
+        public State(BluePrint bluePrint) {
             this.bluePrint = bluePrint;
         }
 
-        void buildOreRobot() {
-            if (ore >= bluePrint.oreRobot.ore) {
-                System.out.println("Building ore robot");
-                ore -= bluePrint.oreRobot.ore;
-                oreRobots++;
-            }
+        public State(int ore, int clay, int obsidian, int openedGeodes, int oreRobots, int clayRobots, int obsidianRobots, int geodeRobots, int time, BluePrint bluePrint) {
+            this.ore = ore;
+            this.clay = clay;
+            this.obsidian = obsidian;
+            this.openedGeodes = openedGeodes;
+            this.oreRobots = oreRobots;
+            this.clayRobots = clayRobots;
+            this.obsidianRobots = obsidianRobots;
+            this.geodeRobots = geodeRobots;
+            this.time = time;
+            this.bluePrint = bluePrint;
         }
 
-        void buildClayRobot() {
-            if (ore >= bluePrint.clayRobot.ore) {
-                System.out.println("Building clay robot");
-                ore -= bluePrint.clayRobot.ore;
-                clayRobots++;
-            }
+        State doNothing() {
+            return new State(
+                    ore + oreRobots,
+                    clay + clayRobots,
+                    obsidian + obsidianRobots,
+                    openedGeodes + geodeRobots,
+                    oreRobots,
+                    clayRobots,
+                    obsidianRobots,
+                    geodeRobots,
+                    time + 1,
+                    bluePrint);
         }
 
-        void buildObsidianRobot() {
-            if (ore >= bluePrint.obsidianRobot.ore && clay >= bluePrint.obsidianRobot.clay) {
-                System.out.println("Building obsidian robot");
-                ore -= bluePrint.obsidianRobot.ore;
-                clay -= bluePrint.obsidianRobot.clay;
-                obsidianRobots++;
-            }
+        State buildOreRobot() {
+            return new State(
+                    ore - bluePrint.oreRobot.ore + oreRobots,
+                    clay + clayRobots,
+                    obsidian + obsidianRobots,
+                    openedGeodes + geodeRobots,
+                    oreRobots + 1,
+                    clayRobots,
+                    obsidianRobots,
+                    geodeRobots,
+                    time + 1,
+                    bluePrint);
+
         }
 
-        void buildGeodeRobot() {
-            if (ore >= bluePrint.geodeRobot.ore && obsidian >= bluePrint.geodeRobot.obsidian) {
-                System.out.println("Building geode robot");
-                ore -= bluePrint.geodeRobot.ore;
-                obsidian -= bluePrint.geodeRobot.obsidian;
-                geodeRobots++;
-            }
+        State buildClayRobot() {
+            return new State(
+                    ore - bluePrint.clayRobot.ore + oreRobots,
+                    clay + clayRobots,
+                    obsidian + obsidianRobots,
+                    openedGeodes + geodeRobots,
+                    oreRobots,
+                    clayRobots + 1,
+                    obsidianRobots,
+                    geodeRobots,
+                    time + 1,
+                    bluePrint);
+        }
+
+        State buildObsidianRobot() {
+            return new State(
+                    ore - bluePrint.obsidianRobot.ore + oreRobots,
+                    clay - bluePrint.obsidianRobot.clay + clayRobots,
+                    obsidian + obsidianRobots,
+                    openedGeodes + geodeRobots,
+                    oreRobots,
+                    clayRobots,
+                    obsidianRobots + 1,
+                    geodeRobots,
+                    time + 1,
+                    bluePrint);
+        }
+
+        State buildGeodeRobot() {
+            return new State(
+                    ore - bluePrint.geodeRobot.ore + oreRobots,
+                    clay + clayRobots,
+                    obsidian - bluePrint.geodeRobot.obsidian + obsidianRobots,
+                    openedGeodes + geodeRobots,
+                    oreRobots,
+                    clayRobots,
+                    obsidianRobots,
+                    geodeRobots + 1,
+                    time + 1,
+                    bluePrint);
         }
 
         @Override
         public String toString() {
-            return "Simulation{" +
-                    "ore=" + ore +
+            return "ore=" + ore +
                     ", clay=" + clay +
                     ", obsidian=" + obsidian +
                     ", openedGeodes=" + openedGeodes +
@@ -125,7 +245,20 @@ public class Day19 extends AbstractTask {
                     ", clayRobots=" + clayRobots +
                     ", obsidianRobots=" + obsidianRobots +
                     ", geodeRobots=" + geodeRobots +
-                    '}';
+                    ", time=" + time;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            State that = (State) o;
+            return ore == that.ore && clay == that.clay && obsidian == that.obsidian && openedGeodes == that.openedGeodes && oreRobots == that.oreRobots && clayRobots == that.clayRobots && obsidianRobots == that.obsidianRobots && geodeRobots == that.geodeRobots;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(ore, clay, obsidian, openedGeodes, oreRobots, clayRobots, obsidianRobots, geodeRobots);
         }
     }
 
