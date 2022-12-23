46cb251488a825e80ebce8e25565e6f83eac3c3e
adampociejowski <valverde12345@gmail.com>
2022-12-23 09:08:48 +0100

    advent of code day 23,22

--- src/test/java/com/valverde/adventofcode2022/Day22.java
+++ src/test/java/com/valverde/adventofcode2022/Day22.java
@@ -3,7 +3,7 @@ package com.valverde.adventofcode2022;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 
 public class Day22 extends AbstractTask {
 
@@ -11,19 +11,228 @@ public class Day22 extends AbstractTask {
     void setup() {
     }
 
+    /*
+      1
+    234
+      56
+     */
     @Test
     void solution2() {
         final List<String> lines = readStringLines("inputTest.txt");
+//        final List<String> lines = readStringLines("input22.txt");
+        Integer width = lines.subList(0, lines.size() - 2)
+                .stream()
+                .map(String::length)
+                .max(Comparator.naturalOrder())
+                .orElseThrow();
+        char[][] grid = getGrid(lines, width);
+        System.out.printf("width: %s, height: %s\n", width, lines.size() - 2);
+
         int result = 0;
         System.out.println("SOLUTION 2: " + result);
     }
 
     @Test
     void solution1() {
-        final List<String> lines = readStringLines("inputTest.txt");
-        int result = 0;
+        final List<String> lines = readStringLines("input22.txt");
+        Integer width = lines.subList(0, lines.size() - 2)
+                .stream()
+                .map(String::length)
+                .max(Comparator.naturalOrder())
+                .orElseThrow();
+        char[][] grid = getGrid(lines, width);
+        Queue<String> commands = getCommands(lines);
+        State state = getStartPosition(grid);
+        List<String> directions = Arrays.asList("R", "D", "L", "U"); //U (up), R (right), D (down), L (left)
+        int iteration = 0;
+        while (!commands.isEmpty()) {
+            String command = commands.poll();
+            System.out.printf("%s: %s - %s\n", ++iteration, command, state);
+            if (command.equals("L")) {
+                int directionIndex = directions.indexOf(state.orientation) - 1 < 0 ?
+                        directions.size() - 1 :
+                        directions.indexOf(state.orientation) - 1;
+                state.orientation = directions.get(directionIndex);
+            } else if (command.equals("R")) {
+                int directionIndex = directions.indexOf(state.orientation) + 1 >= directions.size() ?
+                        0 :
+                        directions.indexOf(state.orientation) + 1;
+                state.orientation = directions.get(directionIndex);
+            } else {
+                int steps = Integer.parseInt(command);
+                if (state.orientation.equals("U")) {
+                    moveUp(grid, steps, state);
+                } else if (state.orientation.equals("R")) {
+                    moveRight(grid, steps, state);
+                } else if (state.orientation.equals("D")) {
+                    moveDown(grid, steps, state);
+                } else {
+                    moveLeft(grid, steps, state);
+                }
+            }
+        }
+        int result = ((state.y + 1) * 1000) + ((state.x + 1) * 4) + directions.indexOf(state.orientation);
+        //3590
         System.out.println("SOLUTION 1: " + result);
     }
 
+    private void moveRight(char[][] grid, int steps, State state) {
+        boolean canMove = true;
+        while (steps > 0 && canMove) {
+            if (state.x + 1 >= grid[0].length || grid[state.y][state.x + 1] == ' ') { //end of map
+                int newX = 0;
+                while (canMove && newX < grid[0].length) {
+                    if (grid[state.y][newX] == '.') {
+                        state.x = newX;
+                        break;
+                    } else if (grid[state.y][newX] == '#') {
+                        canMove = false;
+                    } else {
+                        newX++;
+                    }
+                }
+            } else if (grid[state.y][state.x + 1] == '.') { // can move
+                state.x++;
+            } else { // blockade of road
+                canMove = false;
+            }
+            steps--;
+        }
+    }
 
+    private void moveLeft(char[][] grid, int steps, State state) {
+        boolean canMove = true;
+        while (steps > 0 && canMove) {
+            if (state.x - 1 < 0 || grid[state.y][state.x - 1] == ' ') { //end of map
+                int newX = grid[0].length - 1;
+                while (canMove && newX >= 0) {
+                    if (grid[state.y][newX] == '.') {
+                        state.x = newX;
+                        break;
+                    } else if (grid[state.y][newX] == '#') {
+                        canMove = false;
+                    } else {
+                        newX--;
+                    }
+                }
+            } else if (grid[state.y][state.x - 1] == '.') { // can move
+                state.x--;
+            } else { // blockade of road
+                canMove = false;
+            }
+            steps--;
+        }
+    }
+
+    private void moveUp(char[][] grid, int steps, State state) {
+        boolean canMove = true;
+        while (steps > 0 && canMove) {
+            if (state.y - 1 < 0 || grid[state.y - 1][state.x] == ' ') { //end of map
+                int newY = grid.length - 1;
+                while (canMove && newY >= 0) {
+                    if (grid[newY][state.x] == '.') {
+                        state.y = newY;
+                        break;
+                    } else if (grid[newY][state.x] == '#') {
+                        canMove = false;
+                    } else {
+                        newY--;
+                    }
+                }
+            } else if (grid[state.y - 1][state.x] == '.') { // can move
+                state.y--;
+            } else { // blockade of road
+                canMove = false;
+            }
+            steps--;
+        }
+    }
+
+    private void moveDown(char[][] grid, int steps, State state) {
+        boolean canMove = true;
+        while (steps > 0 && canMove) {
+            if (state.y + 1 >= grid.length || grid[state.y + 1][state.x] == ' ') { //end of map
+                int newY = 0;
+                while (canMove && newY < grid.length) {
+                    if (grid[newY][state.x] == '.') {
+                        state.y = newY;
+                        break;
+                    } else if (grid[newY][state.x] == '#') {
+                        canMove = false;
+                    } else {
+                        newY++;
+                    }
+                }
+            } else if (grid[state.y + 1][state.x] == '.') { // can move
+                state.y++;
+            } else { // blockade of road
+                canMove = false;
+            }
+            steps--;
+        }
+    }
+
+    private State getStartPosition(char[][] grid) {
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[y][x] == '.') {
+                    return new State(x, y);
+                }
+            }
+        }
+        throw new RuntimeException();
+    }
+
+    private char[][] getGrid(List<String> lines, Integer width) {
+        char[][] grid = new char[lines.size() - 2][width];
+        for (int y = 0; y < lines.size() - 2; y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < width; x++) {
+                if (x >= line.length()) {
+                    grid[y][x] = ' ';
+                } else {
+                    grid[y][x] = line.charAt(x);
+                }
+            }
+        }
+        return grid;
+    }
+
+    private Queue<String> getCommands(List<String> lines) {
+        Queue<String> commands = new ArrayDeque<>();
+        StringBuilder currentCommand = new StringBuilder();
+        for (int i = 0; i < lines.get(lines.size() - 1).length(); i++) {
+            char c = lines.get(lines.size() - 1).charAt(i);
+            if (c == 'R' || c == 'L') {
+                if (currentCommand.length() > 0) {
+                    commands.add(currentCommand.toString());
+                    currentCommand = new StringBuilder();
+                }
+                commands.add(String.valueOf(c));
+            } else {
+                currentCommand.append(c);
+            }
+        }
+        if (currentCommand.length() > 0) {
+            commands.add(currentCommand.toString());
+        }
+        return commands;
+    }
+
+
+    static class State {
+        int x;
+        int y;
+        String orientation = "R";
+
+        public State(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(x=" + x +", y=" + y +", orientation='" + orientation+")";
+        }
+    }
 }
--- src/test/java/com/valverde/adventofcode2022/Day23.java
+++ src/test/java/com/valverde/adventofcode2022/Day23.java
@@ -3,7 +3,7 @@ package com.valverde.adventofcode2022;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 
 public class Day23 extends AbstractTask {
 
@@ -13,17 +13,234 @@ public class Day23 extends AbstractTask {
 
     @Test
     void solution2() {
-        final List<String> lines = readStringLines("inputTest.txt");
+        final List<String> lines = readStringLines("input23.txt");
+        char[][] grid = getGrid(lines);
+        Set<Point> elvesLocations = getElvesLocations(grid);
+        Queue<List<String>> directionsQueue = new ArrayDeque<>(Arrays.asList(
+                Arrays.asList("N", "NE", "NW"),
+                Arrays.asList("S", "SE", "SW"),
+                Arrays.asList("W", "NW", "SW"),
+                Arrays.asList("E", "NE", "SE")));
         int result = 0;
+        for (int i = 1; i <= 1000; i++) {
+            Map<Point, List<Point>> newLocationPropositions = new HashMap<>(); // (newLocation, oldLocations)
+            for (Point p : elvesLocations) {
+                if (!hasInAnyDirections(elvesLocations, p, Arrays.asList("N", "NE", "NW", "S", "SE", "SW", "E", "W"))) {
+                    continue;
+                }
+                ArrayDeque<List<String>> queue = new ArrayDeque<>(directionsQueue);
+                boolean looking = true;
+                while (!queue.isEmpty() && looking) {
+                    List<String> directions = queue.poll();
+                    if (!hasInAnyDirections(elvesLocations, p, directions)) {
+                        looking = false;
+                        Point newPointProposition = moveInDirection(p, directions.get(0));
+                        if (!newLocationPropositions.containsKey(newPointProposition)) {
+                            newLocationPropositions.put(newPointProposition, new ArrayList<>(Collections.singletonList(p)));
+                        } else {
+                            newLocationPropositions.get(newPointProposition).add(p);
+                        }
+                    }
+                }
+            }
+            int moves = 0;
+            for (Map.Entry<Point, List<Point>> entry : newLocationPropositions.entrySet()) {
+                if (entry.getValue().size() == 1) {
+                    elvesLocations.remove(entry.getValue().get(0));
+                    elvesLocations.add(entry.getKey());
+                    moves++;
+                }
+            }
+            if (moves == 0) {
+                result = i;
+                break;
+            }
+            List<String> first = directionsQueue.poll();
+            directionsQueue.add(first);
+        }
         System.out.println("SOLUTION 2: " + result);
     }
 
     @Test
     void solution1() {
-        final List<String> lines = readStringLines("inputTest.txt");
-        int result = 0;
+        final List<String> lines = readStringLines("input23.txt");
+        char[][] grid = getGrid(lines);
+        Set<Point> elvesLocations = getElvesLocations(grid);
+        Queue<List<String>> directionsQueue = new ArrayDeque<>(Arrays.asList(
+                Arrays.asList("N", "NE", "NW"),
+                Arrays.asList("S", "SE", "SW"),
+                Arrays.asList("W", "NW", "SW"),
+                Arrays.asList("E", "NE", "SE")));
+        for (int i = 1; i <= 10; i++) {
+            Map<Point, List<Point>> newLocationPropositions = new HashMap<>(); // (newLocation, oldLocations)
+            for (Point p : elvesLocations) {
+                if (!hasInAnyDirections(elvesLocations, p, Arrays.asList("N", "NE", "NW", "S", "SE", "SW", "E", "W"))) {
+                    continue;
+                }
+                ArrayDeque<List<String>> queue = new ArrayDeque<>(directionsQueue);
+                boolean looking = true;
+                while (!queue.isEmpty() && looking) {
+                    List<String> directions = queue.poll();
+                    if (!hasInAnyDirections(elvesLocations, p, directions)) {
+                        looking = false;
+                        Point newPointProposition = moveInDirection(p, directions.get(0));
+                        if (!newLocationPropositions.containsKey(newPointProposition)) {
+                            newLocationPropositions.put(newPointProposition, new ArrayList<>(Collections.singletonList(p)));
+                        } else {
+                            newLocationPropositions.get(newPointProposition).add(p);
+                        }
+                    }
+                }
+            }
+            for (Map.Entry<Point, List<Point>> entry : newLocationPropositions.entrySet()) {
+                if (entry.getValue().size() == 1) {
+                    elvesLocations.remove(entry.getValue().get(0));
+                    elvesLocations.add(entry.getKey());
+                }
+            }
+            List<String> first = directionsQueue.poll();
+            directionsQueue.add(first);
+        }
+        int result = countEmpty(elvesLocations);
         System.out.println("SOLUTION 1: " + result);
     }
 
+    private int countEmpty(Set<Point> points) {
+        MaxMin maxMin = getMaxMin(points);
+        int width = maxMin.maxX - maxMin.minX + 1;
+        int height = maxMin.maxY - maxMin.minY + 1;
+        return (width * height) - points.size();
+    }
 
+    private void print(Set<Point> points) {
+        MaxMin maxMin = getMaxMin(points);
+        for (int y = maxMin.minY; y <= maxMin.maxY; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = maxMin.minX; x <= maxMin.maxX; x++) {
+                if (points.contains(new Point(x, y))) {
+                    sb.append("#");
+                } else {
+                    sb.append(".");
+                }
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+    private MaxMin getMaxMin(Set<Point> points) {
+        MaxMin maxMin = new MaxMin(-1000, 1000, -1000, 1000);
+        for (Point e : points) {
+            if (e.x > maxMin.maxX) {
+                maxMin.maxX = e.x;
+            }
+            if (e.x < maxMin.minX) {
+                maxMin.minX = e.x;
+            }
+            if (e.y > maxMin.maxY) {
+                maxMin.maxY = e.y;
+            }
+            if (e.y < maxMin.minY) {
+                maxMin.minY = e.y;
+            }
+        }
+        return maxMin;
+    }
+
+    class MaxMin {
+        int maxX;
+        int minX;
+        int maxY;
+        int minY;
+
+        public MaxMin(int maxX, int minX, int maxY, int minY) {
+            this.maxX = maxX;
+            this.minX = minX;
+            this.maxY = maxY;
+            this.minY = minY;
+        }
+    }
+
+    private Point moveInDirection(Point p, String direction) {
+        return switch (direction) {
+            case "N" -> new Point(p.x, p.y - 1);
+            case "S" -> new Point(p.x, p.y + 1);
+            case "W" -> new Point(p.x - 1, p.y);
+            case "E" -> new Point(p.x + 1, p.y);
+            default -> throw new RuntimeException();
+        };
+    }
+
+    private boolean hasInAnyDirections(Set<Point> points, Point p, List<String> directions) {
+        for (String direction : directions) {
+            if (hasInDirection(points, p, direction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasInDirection(Set<Point> points, Point p, String direction) {
+        return switch (direction) {
+            case "N" -> points.contains(new Point(p.x, p.y - 1));
+            case "NE" -> points.contains(new Point(p.x + 1, p.y - 1));
+            case "NW" -> points.contains(new Point(p.x - 1, p.y - 1));
+            case "S" -> points.contains(new Point(p.x, p.y + 1));
+            case "SE" -> points.contains(new Point(p.x + 1, p.y + 1));
+            case "SW" -> points.contains(new Point(p.x - 1, p.y + 1));
+            case "W" -> points.contains(new Point(p.x - 1, p.y));
+            case "E" -> points.contains(new Point(p.x + 1, p.y));
+            default -> throw new RuntimeException();
+        };
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x && y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ")";
+        }
+    }
+
+    private Set<Point> getElvesLocations(char[][] grid) {
+        Set<Point> points = new HashSet<>();
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[y][x] == '#') {
+                    points.add(new Point(x, y));
+                }
+            }
+        }
+        return points;
+    }
+
+    private char[][] getGrid(List<String> lines) {
+        char[][] grid = new char[lines.size()][lines.get(0).length()];
+        for (int y = 0; y < lines.size(); y++) {
+            String line = lines.get(y);
+            for (int x = 0; x < grid[0].length; x++) {
+                grid[y][x] = line.charAt(x);
+            }
+        }
+        return grid;
+    }
 }
