3d5f7d6e05d8c2ca21db0e38aa9804e1423f7efa
adampociejowski <valverde12345@gmail.com>
2023-11-30 20:30:36 +0100

    Advent of code 2023 start

---
+++ src/test/java/com/valverde/adventofcode2023/AbstractTask.java
@@ -0,0 +1,65 @@
+package com.valverde.adventofcode2023;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public abstract class AbstractTask {
+
+    Set<String> stringToCharactersSet(String string) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0 ; i < string.length(); i++) {
+            set.add(String.valueOf(string.charAt(i)));
+        }
+        return set;
+    }
+
+    List<String> readStringLines(final String fileName) {
+        final File file = new File("input/adventofcode2023/%s".formatted(fileName));
+        try {
+            return new ArrayList<>(Files
+                    .readAllLines(file.toPath(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    <T> List<List<T>> readLinesSeparatedByEmptyLine(final String fileName, final Class<T> clazz) {
+        final List<String> lines = readStringLines(fileName);
+        final List<List<T>> groups = new ArrayList<>();
+        List<T> currentGroup = new ArrayList<>();
+        for (String line : lines) {
+            if (line.equals("")) {
+                groups.add(currentGroup);
+                currentGroup = new ArrayList<>();
+            } else {
+                if (clazz.equals(Integer.class)) {
+                    Integer i = Integer.parseInt(line);
+                    currentGroup.add((T) i);
+                } else {
+                    currentGroup.add((T) line);
+                }
+            }
+        }
+        groups.add(currentGroup);
+        return groups;
+    }
+
+    List<Integer> readIntLines(final String fileName) {
+        final File file = new File("input/adventofcode2023/%s".formatted(fileName));
+        try {
+            return new ArrayList<>(Files
+                    .readAllLines(file.toPath(), Charset.defaultCharset()))
+                    .stream()
+                    .map(v -> Integer.parseInt(v))
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/valverde/adventofcode2023/Template.java
@@ -0,0 +1,27 @@
+package com.valverde.adventofcode2023;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Template extends AbstractTask {
+
+    @BeforeEach
+    void setup() {
+    }
+
+    @Test
+    void solution1() {
+        final List<String> lines = readStringLines("inputTest.txt");
+
+    }
+
+    @Test
+    void solution2() {
+        final List<String> lines = readStringLines("inputTest.txt");
+
+    }
+}
