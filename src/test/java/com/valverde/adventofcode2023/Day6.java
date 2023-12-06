36e350f91f692a5d2bdb96c6c0ceacde48a18aed
Adam Pociejowski <valverde12345@gmail.com>
2023-12-06 06:22:28 +0100

    Advent of code 2023 day 6

--- src/test/java/com/valverde/adventofcode2023/Day6.java
+++ src/test/java/com/valverde/adventofcode2023/Day6.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Day6 extends AbstractTask {
 
@@ -13,16 +14,72 @@ public class Day6 extends AbstractTask {
 
     @Test
     void solution1() {
-        final List<String> lines = readStringLines("inputTest.txt");
-        int result = 0;
-        System.out.println("Task1: "+result);
+//        final List<String> lines = readStringLines("inputTest.txt");
+        final List<String> lines = readStringLines("input6.txt");
+
+        List<Integer> times = Stream.of(lines
+                .get(0)
+                .replace("Time:", "")
+                .replaceAll(" +", " ")
+                .split(" "))
+                .filter(s -> !s.equals(""))
+                .map(Integer::parseInt)
+                .toList();
+        List<Integer> distances = Stream.of(lines
+                .get(1)
+                .replace("Distance:", "")
+                .replaceAll(" +", " ")
+                .split(" "))
+                .filter(s -> !s.equals(""))
+                .map(Integer::parseInt)
+                .toList();
+
+
+        int result = 1;
+        for (int i = 0; i < times.size(); i++) {
+            int record = distances.get(i);
+            int time = times.get(i);
+            int beatenTimes = 0;
+            for (int t = 1; t < time; t++) {
+                int leftTime = time - t;
+                int distance = leftTime * t;
+                if (distance > record) {
+                    beatenTimes++;
+                }
+            }
+//            if (beatenTimes > 0) {
+//                result *= beatenTimes;
+//            }
+            result *= beatenTimes;
+        }
+
+        System.out.println("Task1: " + result);
 
     }
 
     @Test
     void solution2() {
-        final List<String> lines = readStringLines("inputTest.txt");
+//        final List<String> lines = readStringLines("inputTest.txt");
+        final List<String> lines = readStringLines("input6.txt");
+
+        Long time = Long.valueOf(lines
+                .get(0)
+                .replaceAll("Time: +", "")
+                .replaceAll(" +", ""));
+        Long record = Long.valueOf(lines
+                .get(1)
+                .replaceAll("Distance: +", "")
+                .replaceAll(" +", ""));
+
+
         int result = 0;
-        System.out.println("Task2: "+result);
+        for (Long t = 1L; t < time; t++) {
+            Long leftTime = time - t;
+            Long distance = leftTime * t;
+            if (distance > record) {
+                result++;
+            }
+        }
+        System.out.println("Task2: " + result);
     }
 }
---
+++ src/test/java/com/valverde/adventofcode2023/Day7.java
@@ -0,0 +1,30 @@
+package com.valverde.adventofcode2023;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class Day7 extends AbstractTask {
+
+    @BeforeEach
+    void setup() {
+    }
+
+    @Test
+    void solution1() {
+        final List<String> lines = readStringLines("inputTest.txt");
+//        final List<String> lines = readStringLines("input7.txt");
+        int result = 0;
+        System.out.println("Task1: "+result);
+
+    }
+
+    @Test
+    void solution2() {
+        final List<String> lines = readStringLines("inputTest.txt");
+//        final List<String> lines = readStringLines("input7.txt");
+        int result = 0;
+        System.out.println("Task2: "+result);
+    }
+}
